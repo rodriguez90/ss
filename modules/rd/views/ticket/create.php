1f0482cd0b492e2347beb46770462c9d82907e3d
Pedro Luis Rodriguez Amador <rodriguezamador90@gmail.com>
2018-08-02 13:57:39 -0500

    Fix bug: use left join to resolve ticket, remove link to index ticket from create and update ticket

--- modules/rd/views/ticket/create.php
+++ modules/rd/views/ticket/create.php
@@ -7,7 +7,7 @@ use yii\helpers\Html;
 /* @var $model app\modules\rd\models\Ticket */
 
 $this->title = Yii::t('app', 'Asignación de Cupos');
-$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Cupos'), 'url' => ['index']];
+//$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Cupos'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="ticket-create">
