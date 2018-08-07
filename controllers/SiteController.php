2851592767e07c12e061188d9a5288bf76407304
Pedro Luis Rodriguez Amador <rodriguezamador90@gmail.com>
2018-08-07 13:03:20 -0500

    Fix bug in report query

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -370,8 +370,8 @@ class SiteController extends Controller
                                 COUNT(ticket.id) as countTicket'
                     )
                     ->innerJoin('agency', 'agency.id = process.agency_id')
-                    ->innerJoin("process_transaction","process_transaction.process_id = process.id and 'process_transaction.active'=>1")
-                    ->leftJoin("ticket","process_transaction.id = ticket.process_transaction_id and 'ticket.active'=>1")
+                    ->innerJoin("process_transaction","process_transaction.process_id = process.id and process_transaction.active = 1")
+                    ->leftJoin("ticket","process_transaction.id = ticket.process_transaction_id and ticket.active = 1")
                     ->where(['process.active'=>1])
                     ->andFilterWhere(['process.bl'=>$bl])
                     ->andFilterWhere(['agency_id'=>$agencyId])
